3a6f4e2030878776bec6d8e10bc30fb0363636cb
Vamekh Basharuli <vamekh@brandtotal.com>
2025-10-09 02:46:49 +0400

    [ABSTRACT_FACTORY][UiToolkit](solution) Refactored UiToolkit to implement Abstract Factory pattern by introducing UiToolkitFactory interface and concrete factories (WinUiToolkitFactory, MacosUiToolkitFactory). Enhanced flexibility and eliminated conditional logic for UI component creation.

--- src/test/java/ge/edu/ug/patterns/creational/abstractfactory/uitoolkit/UiToolkitTest.java
+++ src/test/java/ge/edu/ug/patterns/creational/abstractfactory/uitoolkit/UiToolkitTest.java
@@ -2,13 +2,10 @@ package ge.edu.ug.patterns.creational.abstractfactory.uitoolkit;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UiToolkitTest {
     @Test
     public void testUiToolkit() {
-        String environment = "Windows";
-        UiToolkit toolkit = new UiToolkit(environment);
+        WinUiToolkitFactory toolkit = new WinUiToolkitFactory();
 
         toolkit.createButton().render();
         toolkit.createScrollBar().render();
